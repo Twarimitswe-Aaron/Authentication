847da431a8d0e8dd4da12fc0b37986e11a8eb8e8
Twarimitswe Aaron <aarontwarimitswe@gmailcom>
2025-02-28 21:35:43 +0200

    Using debugging tool to see the error of not receiving otp message

--- utils/mailers.js
+++ utils/mailers.js
@@ -13,6 +13,11 @@ const sendOTP= async (email, otp)=>{
                 user: process.env.MJ_APIKEY_PUBLIC,  // Public API Key from Mailjet
                 pass: process.env.MJ_APIKEY_PRIVATE, // Private API Key from Mailjet
             },
+            tls:{
+                rejectUnauthorized:false,
+            },
+            debug:true,
+            logger:true,
         });
 
         console.log("Transporter is created")
@@ -23,6 +28,19 @@ const sendOTP= async (email, otp)=>{
             subject:'OTP for authentication',
             text:`Your OPT code is ${otp}. It is valid for 5 minutes`
         }
+
+        await transporter.verify(function(error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log("Server is ready to send emails",success);
+                console.log("Public Key:", process.env.MJ_APIKEY_PUBLIC);
+                console.log("Private Key:", process.env.MJ_APIKEY_PRIVATE);
+                console.log("Email User:", process.env.EMAIL_USER);
+
+
+            }
+        });
         
 
         try {
@@ -35,6 +53,7 @@ const sendOTP= async (email, otp)=>{
     }catch(err){
        return {success:false, message:err.message};
     }
+ 
 }
 
 
